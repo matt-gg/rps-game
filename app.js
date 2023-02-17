98709cbc83031e9dd982294d5f19288f7f5ca039
matt-gg <94838139+matt-gg@users.noreply.github.com>
2023-02-17 23:40:23 +1100

    Create a basic GUI using HTML buttons

--- app.js
+++ app.js
@@ -1,37 +1,31 @@
 let playerScore = 0;
 let computerScore = 0;
-let roundWinner = '';
+let draws = 0;
+let round = 0;
+const btn = document.querySelectorAll('.btn');
+const gameplay = document.querySelector('.gameplay');
+const score = document.querySelector('.score');
+const result = document.querySelector('.result');
 
-console.log("Let's play Rock Paper Sciossors!");
-
-for (let i = 0; i < 5; i++) {
-    playRound();
-}
-
-gameResult();
+// Game/round is played when the player makes their selection
+btn.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id);
+    });
+});
 
 function getComputerSelection() {
-    let number = Math.floor(Math.random() * 3) + 1;
-    switch (number) {
-        case 1:
-            return 'rock';
-        case 2:
-            return 'paper';
-        case 3:
-            return 'scissors';
-    }
+    const options = ['rock', 'paper', 'scissors'];
+    return options[Math.floor(Math.random() * 3)];
 }
 
-function getPlayerSelection() {
-    let input = prompt("Enter Rock, Paper or Scissors: ");
-    return input;
-}
-
-function playRound() {
-    const playerSelection = getPlayerSelection();
+function playRound(playerChoice) {
+    const playerSelection = playerChoice;
     const computerSelection = getComputerSelection();
 
     game(playerSelection, computerSelection);
+    round++;
+    updateScore();
 }
 
 function game(playerSelection, computerSelection) {
@@ -57,29 +51,31 @@ function game(playerSelection, computerSelection) {
 }
 
 function draw(selection) {
-    roundWinner = 'draw';
-    console.log(`It's a draw! Both players chose ${selection}.`);
+    gameplay.innerHTML = `It's a draw! Both players chose ${selection}.`;
+    draws++;
 }
 
 function playerWinsRound(playerSelection, computerSelection) {
-    console.log(`Player chose ${playerSelection}! Computer chose ${computerSelection}!`);
-    console.log(`Player wins this round!`);
-    roundWinner = 'player';
+    gameplay.innerHTML = `Player chose ${playerSelection}! Computer chose ${computerSelection}!
+    Player wins this round!`;
     playerScore++;
 }
 
 function computerWinsRound(playerSelection, computerSelection) {
-    console.log(`Player chose ${playerSelection}! Computer chose ${computerSelection}!`);
-    console.log(`Computer wins this round!`);
-    roundWinner = 'computer';
+    gameplay.innerHTML = `Player chose ${playerSelection}! Computer chose ${computerSelection}!
+    Computer wins this round!`;
     computerScore++;
 }
 
+function updateScore() {
+    score.innerHTML = `Round ${round} - 
+        You: ${playerScore} -
+        Computer: ${computerScore} -
+        Draws: ${draws}`;
+}
+
 function gameResult() {
-    console.log(`Game over.`);
-    console.log(`FINAL SCORE - Player: ${playerScore} - Computer: ${computerScore}`);
-    
-    if (playerScore === computerScore) console.log(`It's a tie!`);
-    if (playerScore > computerScore) console.log(`Player wins!`);
-    if (playerScore < computerScore) console.log(`Computer wins!`);
+    if (playerScore === computerScore) result.innerHTML = `It's a tie!`;
+    if (playerScore > computerScore) result.innerHTML = `Player wins!`;
+    if (playerScore < computerScore) result.innerHTML = `Computer wins!`;
 }
